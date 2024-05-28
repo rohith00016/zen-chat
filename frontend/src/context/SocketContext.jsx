d54c82f587c4c7eb340a07f24ff4c415181f0271
rohith <rohithm.22mca@kongu.edu>
2024-05-28 10:01:16 +0530

    socket deployed server url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://zen-chat-r71c.onrender.com", {
         query: {
           userId: authUser._id,
         },
